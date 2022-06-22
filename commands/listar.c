eb65c3a3a935b2969ba78bc81a0ce71e3e2d5118
BomberBeetle <gdreks@gmail.com>
2022-06-22 18:35:49 -0300

    listar de verdade real oficial 2016 HD

--- commands/listar.c
+++ commands/listar.c
@@ -21,7 +21,21 @@ int listar (int argc, char* argv[]){
     return RESULT_SUCCESS;
   }
   else {
-    
-    return RESULT_SUCCESS;
+    struct dirent *dir;
+    DIR *d = opendir(argv[1]); 
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            printf("%s - %s\n", dir->d_name, dir->d_type==DT_DIR?"Diretório":"Arquivo");
+        }
+        closedir(d);
+        return RESULT_SUCCESS;
+    }
+    else{
+        printf("Diretório não encontrado.\n");
+        return RESULT_ERROR;
+    }
   }
 }
